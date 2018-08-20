c0a3617f3e3a7633e9da7b76a5731409929d047b
hsb-27 <harjasbathla@gmail.com>
2018-08-20 10:33:54 +0530

    form added

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -17,7 +18,7 @@ def category(request, category_name_slug):
 	context_dict = {}
 
 	try:
-		category = Category.objects.get(slug=category_name_slug)
+		category = Category.objects.get(name=category_name_slug)
 		context_dict['category_name'] = category.name
 		pages = Page.objects.filter(category=category)
 		context_dict['pages'] = pages
@@ -26,3 +27,16 @@ def category(request, category_name_slug):
 		pass	
 
 	return render(request,'rango/category.html',context_dict)	
+
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print (form.errors)
+	else:
+		form = CategoryForm()
+	return render(request, 'rango/add_category.html',{'form':form})					
